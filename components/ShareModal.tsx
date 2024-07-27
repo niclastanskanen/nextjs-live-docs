31c34200b3a1eaa8b640ad983f60e4af719ba14a
Niclas <niclastanskanen@hotmail.com>
2024-07-27 13:00:19 +0200

    feat: Add updateDocumentAccess function to ShareModal component
    This commit adds the updateDocumentAccess function to the ShareModal component. The function is called when the user clicks the share document button and updates the document access for the specified room, email, userType, and updatedBy. This enhancement improves the sharing functionality and allows for better collaboration in the application.

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -17,7 +17,7 @@ import { Label } from './ui/label'
 import { Input } from './ui/input'
 import UserTypeSelector from './UserTypeSelector'
 import Collaborator from './Collaborator'
-
+import { updateDocumentAccess } from '@/lib/actions/room.actions'
 
 const ShareModal = ({ roomId, collaborators, creatorId, currentUserType }: ShareDocumentDialogProps) => {
   const user = useSelf()
@@ -28,7 +28,15 @@ const ShareModal = ({ roomId, collaborators, creatorId, currentUserType }: Share
   const [userType, setUserType ] = useState<UserType>('viewer')
 
   const shareDocumentHandler = async () => {
+    setLoading(true)
 
+    await updateDocumentAccess({
+      roomId,
+      email,
+      userType: userType as UserType,
+      updatedBy: user.info
+    })
+    setLoading(false)
   }
 
   return (
